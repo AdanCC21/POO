e769411727cbebedd18213277e0a620b18f043cc
Andan <141715477+AdanCC21@users.noreply.github.com>
2024-02-09 10:04:31 -0800

    Primera clase

---
+++ DayOne/clases.h
@@ -0,0 +1,10 @@
+#pragma once //Hace que namas lo compile una vez
+
+class Persona {
+    int edad;
+    char name[];
+    void reir();
+    void comer();
+    void saludar();
+    void makereir(int motivo);
+};
---
+++ DayOne/file1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include "clases.h"
+//Para usar lo de c podemos usar el .h o con una c al inicio y removiendo el .h
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+    /*Creamos 2 objetos, existen 2 maneras de crearlo,
+    estatica, el objeto se crea en memoria estatica, en el stack del programa
+    */
+   Persona la_persona; //Es como un struct pa
+    /*En este punto del programa ya existe un objeto del clase persona en la memoria*/
+    
+}
