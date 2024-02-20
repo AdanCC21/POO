5fcb34b583e0da9a4cd75de026018fac5b8f7f25
Andan <141715477+AdanCC21@users.noreply.github.com>
2024-02-20 10:49:24 -0800

    new file:   DayThree/Cheques.h 	new file:   DayThree/Persona.h 	new file:   DayThree/libad.h 	new file:   DayThree/main.cpp 	new file:   DayThree/output/main.exe 	new file:   libad.h 	new file:   list.h

---
+++ DayThree/Persona.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+
+#include <string.h>
+
+#include "Cheques.h"
+#include "libad.h"
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+class persona {    
+    private:
+    char name[50];
+    char app[50];
+    char apm[50];
+    int edad;
+};
---
+++ DayThree/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+#include <string.h>
+
+#include "Cheques.h"
+#include "Persona.h"
+#include "libad.h"
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+int main()
+{
+    srand(time(NULL));
+    int op;
+
+    do
+    {
+        menu();
+        op=valid("Fuera de rango",0,3);
+        switch(op)
+        {
+            case 1:
+            
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 0:
+                cout << "seguro que quieres salir?" << endl << "1.-salir\t2.-Continuar" << endl;
+                cin >> op;
+        }
+    } while (op!=0);
+    
+}
+
+void menu ()
+{
+    cout << "Que acciones desea realizar"<< endl;
+    cout << "1.-Depositar"<< endl;
+    cout << "2.-Retirar" << endl;
+    cout << "3.-Transferencia" << endl;
+    cout << "0.-Salir" << endl;
+}
