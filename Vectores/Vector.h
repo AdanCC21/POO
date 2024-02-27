76e818ccfc0916d0c13328af060515d23a8cf711
Andan <141715477+AdanCC21@users.noreply.github.com>
2024-02-27 10:45:19 -0800

    clase

--- Vectores/Vector.h
+++ Vectores/Vector.h
@@ -1,44 +1,147 @@
 #pragma once
-class Vector
-{
-    public:
-    double x;
-    double y;
-    double z;
-    
-    /*Constructor
-    Utilizamos parametros default Si es que no estan especificadas, 
-    osea si no le enviamos
-    Parametros usara los default*/
-    Vector(double x=0,double y=0,double z=0);
-    Vector Sumar(Vector v2);
-
-
-
-};
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <ostream>
+#include <string>
+#include <sstream> //Streams basados en cadena
+
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
 
 #include "Vector.h"
 
-//Constructor
-Vector::Vector(double x,double y, double z)
+using std::ostream;
+using std::string;
+
+class Vector
+{
+public:
+    double x;
+    double y;
+    double z;
+
+    /*Constructor
+    Utilizamos parametros default Si es que no estan especificadas,
+    osea si no le enviamos
+    Parametros usara los default*/
+    Vector(double x = 0, double y = 0, double z = 0);
+    Vector Sumar(Vector v2);
+    // Para consola void pri();
+    string Tostring();             // Genera una cadena con la informacion
+    Vector Multiplicar(Vector v2); // Producto vectorial
+    Vector Multiplicar(double s);
+
+    float GetMagnitud();
+    Vector GetUnitVector();
+    /*En c++ podemos sobrecargar los operadores, podemos definir que sucedera cuandos se aplique un operador
+    a un elemento de determinado tipo, Para sobrecargar un operador se crea un a funcion con un nombre especial
+    Cuando la funcion es miembro de una clase, el operador izquierdo es el objeto, al cual pertenece la funcion, y el
+    operando derecho debe ser recibido porla funcion como parametro
+    El nombre de la funcion debe ser operator seguido del operador a sobrecargar. el tipo de retorno debe ser congruente
+    con la operacion*/
+
+    Vector operator*(Vector v2);
+    // Vector operator+(double s);
+};
+// Sobrecargamos el operador <<
+//  ostream &operator<<(ostream &out,Vector v);
+
+float x = 10;
+
+// Constructor
+Vector::Vector(double x, double y, double z)
 {
     /*Todo objeto tiene un apuntador a si mismo que se llama
     this. Es una forma de hacer referencia a si mismo*/
-    this->x=x;
-    this->y=y;
-    this->z=z;
+    /*El this es de la variable que manda a llamar la funcion ejemplo v1.sumar(v2), el this se referirira a los valores de v1*/
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    /*el 2 puntos  hace que suemos la x de float global es como para decir que no pertenece a una variable local*/
+    //::x=x;
 }
 
 Vector Vector::Sumar(Vector v2)
 {
     Vector r;
-    r.x=this->x+v2.x;
-    r.y=this->y+v2.y;
-    r.z=this->z+v2.z;
+    r.x = this->x + v2.x;
+    r.y = this->y + v2.y;
+    r.z = this->z + v2.z;
     return r;
 }
+
+string Vector::Tostring()
+{
+
+    /*Para convertir en cadea con c
+    char cad[128];
+    sprintf(cad,"%.2lf,%.2lf,%.2lf",x,y,z);//En lugar de imprimir en pantalla lo imprime en una cadena */
+
+    std::ostringstream out; // Un string de salida de cadena que lo nombramos como out;
+
+    out << "(" << x << "," << y << "," << z << ")"; // En vez de imprimir en pantalla lo imprime en una cadena?
+    // Creo que no es de a wbo el return;
+    return out.str();
+}
+
+Vector Vector::Multiplicar(Vector v2)
+{
+    Vector r;
+    r.x = this->y * v2.z - this->z * v2.y;
+    r.y = this->z * v2.x - this->x * v2.z;
+    r.z = this->x * v2.y - this->y * v2.x;
+    return r;
+}
+
+Vector Vector::Multiplicar(double s)
+{
+    Vector r;
+    r.x = this->x * s;
+    r.y = this->y * s;
+    r.z = this->z * s;
+    return r;
+}
+
+Vector Vector::operator*(Vector v2)
+{
+    Vector r;
+    r.x = this->y * v2.z - this->z * v2.y;
+    r.y = this->z * v2.x - this->x * v2.z;
+    r.z = this->x * v2.y - this->y * v2.x;
+    return r;
+}
+
+float Vector::GetMagnitud()
+{
+    return sqrt(x * x + y * y + z * z);
+}
+
+Vector Vector::GetUnitVector()
+{
+    double m = GetMagnitud();
+    Vector r;
+    r.x = x / m;
+    r.y = y / m;
+    r.z = z / m;
+    return r;
+}
+
+/*
+// Vector Vector::operator+(Vector s)
+// {
+//     Vector r;
+//     r.x=this->x + s;
+//     r.y=this->y + s;
+//     r.z=this->z + s;
+//     return r;
+// }
+*/
+
+/*Es como para cambiar el tipo de print de cout
+y ahorrarnos el v.z y solo enviar v
+ostream &operator<<(ostream &out,Vector v)
+{
+     out<<"("<<v.x<<","<<v.y <<","<<v.z")";
+
+}*/
--- Vectores/main.cpp
+++ Vectores/main.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-
 #include "Vector.h"
 
 using std::cout;
@@ -11,11 +6,32 @@ using std::string;
 
 int main()
 {
-    //v1 x=10, y=20,z=5
+    //Asignacion v1 x=10, y=20,z=5
+    //Son lo mismo (),= {};
     Vector v1(10,20,5),v2={25,11,7};
     Vector v3;
     v3=v1.Sumar(v2);
+    //o
+    //v3=v1.Sumar({20,11,10});Aqui me genera un vector automaticamente
 
-    cout << "vector resultanto: ("<<v3.x << "," <<v3.y << "," << v3.z << ")"; 
+    // cout << "vector resultanto: ("<<v3.x << "," <<v3.y << "," << v3.z << ")"; 
+    cout <<v1.Tostring()<<"+"<<v2.Tostring();
+    cout<<"="<<v3.Tostring()<<endl;
+
+    v3=v1.Multiplicar(v2);
+    //pues lo mismo que:
+    v3=v1*v2;
+    //Seroa lo mismo que usar: 
+    v3=v1.operator*(v2);
+    //operator es una funcion definida en mi clase jsjks
+    cout <<v1.Tostring()<<"+"<<v2.Tostring();
+    cout<<"="<<v3.Tostring()<<endl;
+
+    v3=v3.Multiplicar(2);
+    cout <<v3.Tostring() <<endl;
+
+    //Con esto le asigna el valor automatico depende lo que le guardemos
+    // auto xvar=10.5;
+    
 }
 
