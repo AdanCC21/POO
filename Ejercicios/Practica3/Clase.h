d20a2ffb7e6456d74c9f552e616d9d5a35de0657
Andan <141715477+AdanCC21@users.noreply.github.com>
2024-02-28 09:50:54 -0800

    a

---
+++ Ejercicios/Practica3/Clase.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+#pragma once
---
+++ Ejercicios/Practica3/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "libad.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    // int a[16]={1,5,3,6,7,3,5,3,7,12,53,23,5634,32,4,32};
+    // for (int n : a)
+    //     cout<< n <<" ";
+    // cout<<endl;
+
+
+}
