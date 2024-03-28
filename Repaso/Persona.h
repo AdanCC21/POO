a2ec61df39a5796dbffe6110c0934f23599b161b
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2024-03-27 18:39:40 -0700

    modified:   .vscode/settings.json 	deleted:    Ejercicios/Practica3v2/frac.h 	deleted:    Ejercicios/Practica3v2/main.cpp 	new file:   Repaso/Persona.h 	new file:   Repaso/main.cpp 	new file:   Repaso/output/main.exe

---
+++ Repaso/Persona.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+class Persona{
+private:
+    string name;
+    int edad;
+public:
+    Persona();
+    Persona(string name, int edad);
+    string getName();
+    int getEdad();
+};
+
+Persona::Persona(string _name, int _edad)
+{
+    name=_name;
+    edad=_edad;
+}
+
+string Persona::getName()
+{
+    return name;   
+}
+
+int Persona::getEdad()
+{
+    return edad;
+}
---
+++ Repaso/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+#include "Persona.h"
+
+int main()
+{
+    string name;
+    int edad;
+    
+    cout<< "Ingrese su nombre"<<endl;
+    cin>>name;
+    cout<<"Ingrese su edad"<<endl;
+    cin>>edad;
+    
+    Persona per1 (name,edad);    
+    cout<<"Datos"<<endl;
+    cout<<per1.getName()<<endl<<per1.getEdad()<<endl;
+}
